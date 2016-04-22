fd5e052fc397773fb8f6502a41416914ef1dbbff
Legatofseal <legatofseal@gmail.com>
2016-04-22 14:43:34 +0300

    Hi

---
+++ src/com/company/EvenOddComparator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.*;
+
+/**
+ * Created by Legat on 22.04.2016.
+ */
+public class EvenOddComparator implements java.util.Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        return 0;
+    }
+    public int compare(int i1, int i2) {
+
+        return 0;
+    }
+}
